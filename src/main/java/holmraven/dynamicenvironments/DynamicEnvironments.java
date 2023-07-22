09b08b70b7974c51ac7677cd0666b8a54e22a6da
holmraven <16858119+holmraven@users.noreply.github.com>
2023-07-22 03:42:21 +0300

    improvements

--- src/main/java/holmraven/dynamicenvironments/DynamicEnvironments.java
+++ src/main/java/holmraven/dynamicenvironments/DynamicEnvironments.java
@@ -6,7 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DynamicEnvironments implements ModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("dynamic-environments");
+	public static final String MODID = "dynamic-environments";
+    public static final Logger LOGGER = LoggerFactory.getLogger(DynamicEnvironments.MODID);
 
 	@Override
 	public void onInitialize() {
